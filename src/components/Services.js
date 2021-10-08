8beaeac9c192534f56f70a0f774a13aac9e61f39
Ashishvajpayee <ashishvajpayee19cs400@bnmit.in>
2021-10-08 16:14:50 +0530

    About Page Image Fixed

--- src/components/Services.js
+++ src/components/Services.js
@@ -1,4 +1,7 @@
 import Card from "./Card";
+import Card1 from "./img/card1.png";
+import Card2 from "./img/card2.png";
+import Card3 from "./img/card3.png";
 function Services() {
   return (
     <div className="container services">
@@ -7,13 +10,13 @@ function Services() {
             <div className="col-md-12">
                 <div className="row">
                     <div className="col-md-4 mb-2">
-                        <Card title="Web Development" img="card1.png" text="Complete website building and managing for every business and industry." />
+                        <Card title="Web Development" img={Card1} text="Complete website building and managing for every business and industry." />
                     </div>
                     <div className="col-md-4 mb-2">
-                    <Card title="E-Commerce Services" img="card2.png" text="Managing E-Commerce portal and the maintaince of App and Services" />
+                    <Card title="E-Commerce Services" img={Card2} text="Managing E-Commerce portal and the maintaince of App and Services" />
                     </div>
                     <div className="col-md-4 mb-2">
-                    <Card title="Cyber Security" img="card3.png" text="Working with Financial Sectors to banking applications Providing Security to each and Every Aspects of Industry needs." />
+                    <Card title="Cyber Security" img={Card3} text="Working with Financial Sectors to banking applications Providing Security to each and Every Aspects of Industry needs." />
                     </div>
                 </div>
             </div>
